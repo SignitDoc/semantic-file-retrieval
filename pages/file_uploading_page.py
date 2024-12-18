11176e04226dc399f381beba03b7386b196f2cc3
tanzhenwei <919548663@qq.com>
2024-12-18 11:19:22 +0800

    优化上传交互

--- pages/file_uploading_page.py
+++ pages/file_uploading_page.py
@@ -12,32 +12,40 @@ uploaded_file = st.file_uploader(
     type=["jpg", "jpeg", "png", "txt", "md", "pdf"],
 )
 uploaded_file_path = ""
-with st.spinner("正在上传..."):
-    if uploaded_file is not None:
-        uploaded_file_path = os.path.join(UPLOAD_DIR, uploaded_file.name)
-        file_ext = get_file_ext(uploaded_file_path)
-        with open(uploaded_file_path, "wb") as f:
-            # 保存到本地
-            f.write(uploaded_file.getvalue())
+if uploaded_file is not None:
+    file_name = uploaded_file.name
+    files = os.listdir(UPLOAD_DIR)
+    if file_name in files:
+        st.error(f"文件 {file_name} 已存在")
+    else:
+        with st.status("上传中，请稍等...", expanded=True) as status:
+            st.write("1.正在保存文件...")
+            uploaded_file_path = os.path.join(UPLOAD_DIR, file_name)
+            file_ext = get_file_ext(uploaded_file_path)
+            with open(uploaded_file_path, "wb") as f:
+                # 保存到本地
+                f.write(uploaded_file.getvalue())
 
-        document_content = ""
-        if file_ext in [".jpg", ".jpeg", ".png"]:
-            document_content = parse_image(uploaded_file_path)
-        elif file_ext in [".txt", ".md"]:
-            document_content = parse_txt_or_md(uploaded_file_path)
-        elif file_ext == ".pdf":
-            document_content = parse_pdf(uploaded_file_path)
-        else:
-            st.error("暂不支持该文件类型!")
+            st.write("2.正在解析文件内容...")
+            document_content = ""
+            if file_ext in [".jpg", ".jpeg", ".png"]:
+                document_content = parse_image(uploaded_file_path)
+            elif file_ext in [".txt", ".md"]:
+                document_content = parse_txt_or_md(uploaded_file_path)
+            elif file_ext == ".pdf":
+                document_content = parse_pdf(uploaded_file_path)
+            else:
+                st.error("暂不支持该文件类型!")
 
-        if document_content:
-            embedding = get_embedding(document_content)
-            my_collection.add(
-                documents=[document_content],
-                embeddings=[embedding],
-                ids=[uploaded_file.name],  # 暂时用文件名作为id
-            )
-        st.toast(f"文件 {uploaded_file.name} 上传成功", icon="✅")
+            st.write("3.正在创建向量索引...")
+            if document_content:
+                embedding = get_embedding(document_content)
+                my_collection.add(
+                    documents=[document_content],
+                    embeddings=[embedding],
+                    ids=[file_name],  # 暂时用文件名作为id
+                )
+            status.update(label="上传成功!", state="complete", expanded=False)
 
 st.divider()
 
